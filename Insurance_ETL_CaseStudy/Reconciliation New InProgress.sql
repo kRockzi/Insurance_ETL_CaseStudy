996815301086f93280c4ddfe048c4dd26a69dce0
kRockzi <81424121+kRockzi@users.noreply.github.com>
2026-06-19 23:21:08 +0530

    Update Reconciliation New InProgress.sql
    In progress

--- Insurance_ETL_CaseStudy/Reconciliation New InProgress.sql
+++ Insurance_ETL_CaseStudy/Reconciliation New InProgress.sql
@@ -1,11 +1,36 @@
 --  reconciliation logic rebuilding in progress 
 
+-- issue :  ghost warnings 
+ -- WITH Missing_Data_Check AS (
+--     -- The Pure Math Engine
+--     SELECT DISTINCT POL_NBR AS Missing_Policies 
+--     FROM dbo.STG_POLICY
+
+--     EXCEPT 
+
+--     (
+--         SELECT POLICY_NUMBER FROM dbo.ILM_POLICY
+--         UNION 
+--         SELECT POL_NBR FROM dbo.ERROR_POLICY
+--     )
+-- )
+-- -- SCENARIO A: The query found missing data
+-- SELECT 
+--     'WARNING: Missing Record Detected' AS Alert_Message,
+--     'Investigate source data immediately' AS Recommended_Action,
+--     Missing_Policies
+-- FROM Missing_Data_Check
+
+-- UNION ALL
+
+-- -- SCENARIO B: The query found absolutely nothing
+-- SELECT 
+--     'SUCCESS: Zero Drops' AS Alert_Message,
+--     'Pipeline balanced perfectly' AS Recommended_Action,
+--     'NONE' AS Missing_Policies
+-- WHERE NOT EXISTS (SELECT 1 FROM Missing_Data_Check);
+-- GO
 
 
-SELECT DISTINCT POL_NBR FROM dbo.STG_POLICY
-EXCEPT 
-(
-    SELECT POLICY_NUMBER FROM dbo.ILM_POLICY
-    UNION 
-    SELECT POL_NBR FROM dbo.ERROR_POLICY
-);
+
+     
